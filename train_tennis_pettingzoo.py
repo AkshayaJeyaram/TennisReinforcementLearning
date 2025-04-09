3dd807d7d26c90ec0df877adf952f9d03424218b
Akshaya Jeyaram <akshayajeyaram@172-26-11-120.eduroam.bath.ac.uk>
2025-04-09 14:17:28 +0100

    Initial commit - Unity Tennis env with Python script

---
+++ train_tennis_pettingzoo.py
@@ -0,0 +1,19 @@
+
+pip install 'pettingzoo[atari]'
+pip install "autorom[accept-rom-license]"
+from pettingzoo.atari import tennis_v3
+
+env = tennis_v3.env(render_mode = 'human')
+env.reset(seed=42)
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+
+    if termination or truncation:
+        action = None
+    else:
+        # this is where you would insert your policy
+        action = env.action_space(agent).sample()
+
+    env.step(action)
+env.close()
